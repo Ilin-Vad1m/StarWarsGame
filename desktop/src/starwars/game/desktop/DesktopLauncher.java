3440c9b5fdac8942aefb388f9d0f7ce191cbeb91
Ilin-Vad1m <vad2.com@gmail.com>
2020-02-25 02:32:36 +0300

    проект собран!

---
+++ desktop/src/starwars/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package starwars.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import starwars.game.StarWarsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarWarsGame(), config);
+	}
+}
