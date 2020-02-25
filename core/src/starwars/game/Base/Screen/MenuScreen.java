e3aa74959e7700471fc4d3cccee255bf8328ee01
Ilin-Vad1m <vad2.com@gmail.com>
2020-02-25 17:27:56 +0300

    Добавили анимацию картинки

--- core/src/starwars/game/Base/Screen/MenuScreen.java
+++ core/src/starwars/game/Base/Screen/MenuScreen.java
@@ -3,17 +3,25 @@ package starwars.game.Base.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 import starwars.game.Base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
+    private Vector2 touch;
+    private Vector2 v;
+    private Vector2 pos;
+
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("stars.jpg");
+        img = new Texture("badlogic.jpg");
+        touch = new Vector2();
+        v = new Vector2(1f, 1f);
+        pos = new Vector2(0,0);
     }
 
     @Override
@@ -21,8 +29,11 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(pos.y + img.getHeight() < Gdx.graphics.getHeight()){
+            pos.add(v);
+        }
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
     }
 
@@ -30,5 +41,15 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         img.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+       super.touchDown(screenX, screenY, pointer, button);
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        System.out.println("touchX = " + touch.x + " touchY = " + touch.y);
+        System.out.println(pos.y + img.getHeight());
+        return false;
+
     }
 }
