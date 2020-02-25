b9da49bd748bc427d6ce2823bdd2555d43c44665
Ilin-Vad1m <vad2.com@gmail.com>
2020-02-26 00:56:40 +0300

    Домашнее задание! Картинка движется за мышкой!

--- core/src/starwars/game/Base/Screen/MenuScreen.java
+++ core/src/starwars/game/Base/Screen/MenuScreen.java
@@ -13,24 +13,30 @@ public class MenuScreen extends BaseScreen {
     private Vector2 touch;
     private Vector2 v;
     private Vector2 pos;
+    private static final float V_LEN = 1f;
+    private Vector2 buf;
 
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
+        img = new Texture("images.jpg");
         touch = new Vector2();
-        v = new Vector2(1f, 1f);
+        v = new Vector2();
         pos = new Vector2(0,0);
+        buf = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if(pos.y + img.getHeight() < Gdx.graphics.getHeight()){
+        buf.set(touch);
+        if(buf.sub(pos).len() > V_LEN) {
             pos.add(v);
+        }else {
+            pos.set(touch);
         }
         batch.begin();
         batch.draw(img, pos.x, pos.y);
@@ -47,6 +53,7 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
        super.touchDown(screenX, screenY, pointer, button);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(V_LEN));
         System.out.println("touchX = " + touch.x + " touchY = " + touch.y);
         System.out.println(pos.y + img.getHeight());
         return false;
